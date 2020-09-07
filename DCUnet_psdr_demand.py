3f484296830be14916748b0bb10b04b53daca9bf
HJW <hju0518@sogang.ac.kr>
2020-09-07 17:11:44 +0900

    DCUnet upload

---
+++ DCUnet_psdr_demand.py
@@ -0,0 +1,395 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchaudio
+import librosa
+import numpy
+class ComplexConv2d(nn.Module):
+    # https://github.com/litcoderr/ComplexCNN/blob/master/complexcnn/modules.py
+    def __init__(self, in_channel, out_channel, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, **kwargs):
+        super().__init__()
+
+        ## Model components
+        self.conv_re = nn.Conv2d(in_channel, out_channel, kernel_size, stride=stride, padding=padding,
+                                 dilation=dilation, groups=groups, bias=bias, **kwargs)
+        self.conv_im = nn.Conv2d(in_channel, out_channel, kernel_size, stride=stride, padding=padding,
+                                 dilation=dilation, groups=groups, bias=bias, **kwargs)
+
+    def forward(self, x):  # shpae of x : [batch,channel,axis1,axis2,2]
+        real = self.conv_re(x[..., 0]) - self.conv_im(x[..., 1])
+        imaginary = self.conv_re(x[..., 1]) + self.conv_im(x[..., 0])
+        output = torch.stack((real, imaginary), dim=-1)
+        return output
+
+
+class ComplexConvTranspose2d(nn.Module):
+    def __init__(self, in_channel, out_channel, kernel_size, stride=1, padding=0, output_padding=0, dilation=1, groups=1, bias=True, **kwargs):
+        super().__init__()
+
+        ## Model components
+        self.tconv_re = nn.ConvTranspose2d(in_channel, out_channel,
+                                           kernel_size=kernel_size,
+                                           stride=stride,
+                                           padding=padding,
+                                           output_padding=output_padding,
+                                           groups=groups,
+                                           bias=bias,
+                                           dilation=dilation,
+                                           **kwargs)
+        self.tconv_im = nn.ConvTranspose2d(in_channel, out_channel,
+                                           kernel_size=kernel_size,
+                                           stride=stride,
+                                           padding=padding,
+                                           output_padding=output_padding,
+                                           groups=groups,
+                                           bias=bias,
+                                           dilation=dilation,
+                                           **kwargs)
+
+    def forward(self, x):  # shpae of x : [batch,channel,axis1,axis2,2]
+        real = self.tconv_re(x[..., 0]) - self.tconv_im(x[..., 1])
+        imaginary = self.tconv_re(x[..., 1]) + self.tconv_im(x[..., 0])
+        output = torch.stack((real, imaginary), dim=-1)
+        return output
+
+
+class ComplexBatchNorm2d(nn.Module):
+    def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=True,
+                 track_running_stats=True, **kwargs):
+        super().__init__()
+        self.bn_re = nn.BatchNorm2d(num_features=num_features, momentum=momentum, affine=affine, eps=eps, track_running_stats=track_running_stats, **kwargs)
+        self.bn_im = nn.BatchNorm2d(num_features=num_features, momentum=momentum, affine=affine, eps=eps, track_running_stats=track_running_stats, **kwargs)
+
+    def forward(self, x):
+        real = self.bn_re(x[..., 0])
+        imag = self.bn_im(x[..., 1])
+        output = torch.stack((real, imag), dim=-1)
+        return output
+
+
+
+
+class Encoder(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding=None, complex=False, padding_mode="zeros"):
+        super().__init__()
+        if padding is None:
+            padding = [(i - 1) // 2 for i in kernel_size]  # 'SAME' padding
+            
+        if complex:
+            conv = ComplexConv2d
+            bn = ComplexBatchNorm2d
+        else:
+            conv = nn.Conv2d
+            bn = nn.BatchNorm2d
+
+        self.conv = conv(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, padding_mode=padding_mode)
+        self.bn = bn(out_channels)
+        self.relu = nn.LeakyReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, output_padding,padding=(0, 0), complex=False):
+        super().__init__()
+        if complex:
+            tconv = ComplexConvTranspose2d
+            bn = ComplexBatchNorm2d
+        else:
+            tconv = nn.ConvTranspose2d
+            bn = nn.BatchNorm2d
+        
+        self.transconv = tconv(in_channels, out_channels, kernel_size=kernel_size, stride=stride, output_padding=output_padding,padding=padding)
+        self.bn = bn(out_channels)
+        self.relu = nn.LeakyReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.transconv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        return x
+
+
+class UNet(nn.Module):
+    def __init__(self, input_channels=1,
+                 complex=True,
+                 model_complexity=45,
+                 model_depth=20,
+                 padding_mode="zeros"):
+        super().__init__()
+
+        if complex:
+            model_complexity = int(model_complexity // 1.414)
+
+        self.set_size(model_complexity=model_complexity, input_channels=input_channels, model_depth=model_depth)
+        self.encoders = []
+        self.model_length = model_depth // 2
+
+        for i in range(self.model_length):
+            module = Encoder(self.enc_channels[i], self.enc_channels[i + 1], kernel_size=self.enc_kernel_sizes[i],
+                             stride=self.enc_strides[i], padding=self.enc_paddings[i], complex=complex, padding_mode=padding_mode)
+            self.add_module("encoder{}".format(i), module)
+            self.encoders.append(module)
+
+        self.decoders = []
+
+        for i in range(self.model_length):
+            module = Decoder(self.dec_channels[i] + self.enc_channels[self.model_length - i], self.dec_channels[i + 1], kernel_size=self.dec_kernel_sizes[i],
+                             stride=self.dec_strides[i], padding=self.dec_paddings[i], output_padding=self.dec_output_paddings[i],complex=complex)
+            self.add_module("decoder{}".format(i), module)
+            self.decoders.append(module)
+
+        if complex:
+            conv = ComplexConv2d
+        else:
+            conv = nn.Conv2d
+
+        linear = conv(self.dec_channels[-1], 1, 1)
+
+        self.add_module("linear", linear)
+        self.complex = complex
+        self.padding_mode = padding_mode
+
+        self.decoders = nn.ModuleList(self.decoders)
+        self.encoders = nn.ModuleList(self.encoders)
+
+    def forward(self, real,imag):
+
+
+        real_spec = real
+        imag_spec = imag
+        
+        real_spec1 = torch.unsqueeze(real_spec,1) #B 1 F T]
+        imag_spec1 = torch.unsqueeze(imag_spec,1) #B 1 F T]
+        cmp_spec = torch.cat([real_spec1,imag_spec1],1) #[B 2 F T]
+        cmp_spec = torch.unsqueeze(cmp_spec,1) #[B 1 2 F T]
+        cmp_spec = cmp_spec.permute(0,1,3,4,2)
+        if self.complex:
+            x = cmp_spec
+        else:
+            x = bd['mag_X']
+        # go down
+        xs = []
+        for i, encoder in enumerate(self.encoders):
+            xs.append(x)
+            
+            x = encoder(x)
+            #print("x{}".format(i), x.shape)
+        # xs : x0=input x1 ... x9
+
+        #print(x.shape)
+        p = x
+        for i, decoder in enumerate(self.decoders):
+            p = decoder(p)
+            if i == self.model_length - 1:
+                break
+            #print(f"p{i}, {p.shape} + x{self.model_length - 1 - i}, {xs[self.model_length - 1 -i].shape}, padding {self.dec_paddings[i]}")
+            
+            p = torch.cat([p, xs[self.model_length - 1 - i]], dim=1)
+
+        #print(p.shape)
+        mask = self.linear(p)
+        mask = torch.tanh(mask)
+        mask = torch.squeeze(mask,1)
+        
+        #bd['M_hat'] = mask
+        return real_spec*mask[:,:,:,0], imag_spec*mask[:,:,:,1]
+
+    def set_size(self, model_complexity, model_depth=20, input_channels=1):
+        if model_depth == 10:
+            self.enc_channels = [input_channels,
+                                 model_complexity,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 ]
+            self.enc_kernel_sizes = [(7, 5),
+                                     (7, 5),
+                                     (5, 3),
+                                     (5, 3),
+                                     (5, 3)]
+            self.enc_strides = [(2, 2),
+                                (2, 2),
+                                (2, 2),
+                                (2, 2),
+                                (2, 1)]
+            self.enc_paddings = [(2, 1),
+                                 None,
+                                 None,
+                                 None,
+                                 None]
+
+            self.dec_channels = [0,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2]
+
+            self.dec_kernel_sizes = [(4, 3),
+                                     (4, 4),
+                                     (6, 4),
+                                     (6, 4),
+                                     (7, 5)]
+
+            self.dec_strides = [(2, 1),
+                                (2, 2),
+                                (2, 2),
+                                (2, 2),
+                                (2, 2)]
+
+            self.dec_paddings = [(1, 1),
+                                 (1, 1),
+                                 (2, 1),
+                                 (2, 1),
+                                 (2, 1)]
+
+        elif model_depth == 20:
+            self.enc_channels = [input_channels,
+                                 model_complexity,
+                                 model_complexity,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 128]
+
+            self.enc_kernel_sizes = [(7, 1),
+                                     (1, 7),
+                                     (7, 5),
+                                     (7, 5),
+                                     (5, 3),
+                                     (5, 3),
+                                     (5, 3),
+                                     (5, 3),
+                                     (5, 3),
+                                     (5, 3)]
+
+            self.enc_strides = [(1, 1),
+                                (1, 1),
+                                (2, 2),
+                                (2, 1),
+                                (2, 2),
+                                (2, 1),
+                                (2, 2),
+                                (2, 1),
+                                (2, 2),
+                                (2, 1)]
+
+            self.enc_paddings = [(3, 0),
+                                 (0, 3),
+                                 (3, 2),
+                                 (3, 2),
+                                 (2, 1),
+                                 (2, 1),
+                                 (2, 1),
+                                 (2, 1),
+                                 (2, 1),
+                                 (2, 1),]
+                              
+                                 
+
+            self.dec_channels = [0,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2,
+                                 model_complexity * 2]
+
+            self.dec_kernel_sizes = [(5, 3),
+                                     (5, 3),
+                                     (5, 3),
+                                     (5, 3),
+                                     (5, 3),
+                                     (5, 3), 
+                                     (7, 5), 
+                                     (7, 5), 
+                                     (1, 7),
+                                     (7, 1)]
+
+            self.dec_strides = [(2, 1),
+                                (2, 2),
+                                (2, 1),
+                                (2, 2),
+                                (2, 1),
+                                (2, 2),
+                                (2, 1),
+                                (2, 2),
+                                (1, 1),
+                                (1, 1)]
+
+            self.dec_paddings = [(2, 1),
+                                 (2, 1),
+                                 (2, 1),
+                                 (2, 1),
+                                 (2, 1),
+                                 (2, 1),
+                                 (3, 2),
+                                 (3, 2),
+                                 (0, 3),
+                                 (3, 0)]
+            self.dec_output_paddings = [(0,0),
+                                        (0,1),
+                                        (0,0),
+                                        (0,1),
+                                        (0,0),
+                                        (0,1),
+                                        (0,0),
+                                        (0,1),
+                                        (0,0),
+                                        (0,0)]
+        else:
+            raise ValueError("Unknown model depth : {}".format(model_depth))
+
+def wSDRLoss(mixed, clean, clean_est, eps=2e-7):
+    # Used on signal level(time-domain). Backprop-able istft should be used.
+    # Batched audio inputs shape (N x T) required.
+    bsum = lambda x: torch.sum(x, dim=1) # Batch preserving sum for convenience.
+    def mSDRLoss(orig, est):
+        # Modified SDR loss, <x, x`> / (||x|| * ||x`||) : L2 Norm.
+        # Original SDR Loss: <x, x`>**2 / <x`, x`> (== ||x`||**2)
+        #  > Maximize Correlation while producing minimum energy output.
+        correlation = bsum(orig * est)
+        energies = torch.norm(orig, p=2, dim=1) * torch.norm(est, p=2, dim=1)
+        return -(correlation / (energies + eps))
+
+    noise = mixed - clean
+    noise_est = mixed - clean_est
+
+    a = bsum(clean**2) / (bsum(clean**2) + bsum(noise**2) + eps)
+    #wSDR = a * mSDRLoss(clean, clean_est) + (1 - a) * mSDRLoss(noise, noise_est)
+    wSDR = mSDRLoss(clean, clean_est)
+    return torch.mean(wSDR)
+        
+def l2_norm(s1, s2):
+    #norm = torch.sqrt(torch.sum(s1*s2, 1, keepdim=True))
+    #norm = torch.norm(s1*s2, 1, keepdim=True)
+    
+    norm = torch.sum(s1*s2, -1, keepdim=True)
+    return norm 
+
+def si_snr(s1, s2, eps=1e-8):
+    #s1 = remove_dc(s1)
+    #s2 = remove_dc(s2)
+    s1_s2_norm = l2_norm(s1, s2)
+    s2_s2_norm = l2_norm(s2, s2)
+    s_target =  s1_s2_norm/(s2_s2_norm+eps)*s2
+    e_nosie = s1 - s_target
+    target_norm = l2_norm(s_target, s_target)
+    noise_norm = l2_norm(e_nosie, e_nosie)
+    snr = 10*torch.log10((target_norm)/(noise_norm+eps)+eps)
+    return -torch.mean(snr)
