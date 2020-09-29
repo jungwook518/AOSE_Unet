93da7769fba22f18fdc4fb5ab7367c3f78dc0756
HJW <hju0518@sogang.ac.kr>
2020-09-29 17:29:41 +0900

    modi

--- test.sh
+++ test.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+
+
+fs='48'
+test_model='model_ckpt/bestmodel.pth'
+test_data_txt='/home/nas/user/jungwook/fairseq/examples/audio_visual_speech_enhancement/Magnitude_subnetwork/sample/sample_data.txt'
+test_data_output_path='/home/nas/user/jungwook/fairseq/examples/audio_visual_speech_enhancement/Magnitude_subnetwork/sample_output'
+
 python test_DCUnet_jsdr_demand.py \
---fs 48 --test_model model_ckpt/bestmodel.pth \
---test_data_txt /home/nas/user/jungwook/fairseq/examples/audio_visual_speech_enhancement/Magnitude_subnetwork/sample/sample_data.txt \
---test_data_output_path /home/nas/user/jungwook/fairseq/examples/audio_visual_speech_enhancement/Magnitude_subnetwork/sample_output
+--fs $fs --test_model $test_model \
+--test_data_txt $test_data_txt \
+--test_data_output_path $test_data_output_path
