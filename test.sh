d92bd88783b28706a663dec294f6fbc7aab2b2b7
HJW <hju0518@sogang.ac.kr>
2020-09-29 14:46:31 +0900

    modi

---
+++ test.sh
@@ -0,0 +1,1 @@
+python test_DCUnet_jsdr_demand.py  --fs 48 --test_model model_ckpt/bestmodel.pth --test_data_txt /home/nas/user/jungwook/fairseq/examples/audio_visual_speech_enhancement/Magnitude_subnetwork/sample/sample_data.txt --test_data_output_path /home/nas/user/jungwook/fairseq/examples/audio_visual_speech_enhancement/Magnitude_subnetwork/sample_output
