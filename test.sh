face028cc752a12d28ef6c88cb5eb8daeda61a4d
HJW <hju0518@sogang.ac.kr>
2020-09-30 12:43:29 +0900

    modi

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-fs='44.100' ##16 32 48 44.100, 22.050,..... Other than 16,32,48, sr is automatically changed to 16.
+fs='44.100' ##16 32 48 44.100, 22.050,..... Other than 16,32,48, sr is automatically changed to nearest fs.
 test_model='model_ckpt/bestmodel.pth'
 test_data_root_folder='/home/nas/user/jungwook/fairseq/examples/audio_visual_speech_enhancement/Magnitude_subnetwork/sample_audio/'
 test_data_output_path='/home/nas/user/jungwook/fairseq/examples/audio_visual_speech_enhancement/Magnitude_subnetwork/sample_output'
