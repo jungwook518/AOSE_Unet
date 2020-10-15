590b6219eafafbd8108b8a03ced736d53eda3455
HJW <hju0518@sogang.ac.kr>
2020-10-15 19:56:11 +0900

    modi

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-fs='44.1' ##16 32 48 44.100, 22.050,..... Other than 16,32,48, sr is automatically changed to nearest fs.
+fs='16' ##16 32 48 44.100, 22.050,..... Other than 16,32,48, sr is automatically changed to nearest fs.
 test_model='model_ckpt/bestmodel.pth'
-test_data_root_folder='../sample_441/'
+test_data_root_folder='../sample_16/'
 test_data_output_path='/home/nas/user/jungwook/fairseq/examples/audio_visual_speech_enhancement/Magnitude_subnetwork/sample_output'
 
 python test_DCUnet_jsdr_demand.py \
--- test_DCUnet_jsdr_demand.py
+++ test_DCUnet_jsdr_demand.py
@@ -12,7 +12,7 @@ from fairseq import utils
 from DCUnet_jsdr_demand import *
 import librosa
 from tensorboardX import SummaryWriter
-from dataset.demand_dataset_test_dir import *
+from dataset.demand_dataset_test_librosa import *
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--fs',type=float,required=True)
@@ -35,7 +35,7 @@ def complex_demand_audio(complex_ri,window,length,fs):
     complex_ri = complex_ri
     fs=fs
     audio = torchaudio.functional.istft(stft_matrix = complex_ri, n_fft=int(1024*fs), hop_length=int(256*fs), win_length=int(1024*fs), window=window, center=True, pad_mode='reflect', normalized=False, onesided=True, length=length)
-    audio = audio.numpy().squeeze()
+    #audio = audio.numpy().squeeze()
     return audio
 
 def find_nearest(array,value):
@@ -60,7 +60,9 @@ if __name__ == '__main__':
 
     
     args = get_args()
-    device = torch.device('cpu')
+    os.environ["CUDA_VISIBLE_DEVICES"]='0'
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    #device = torch.device('cpu')
     fs = args.fs/16 #16,32,48
     orig_fs = args.fs/16
     batch_size = 1
@@ -82,19 +84,17 @@ if __name__ == '__main__':
     data_test_list=search(data_test,data_test_list)
     test_data_output_path = args.test_data_output_path
     test_dataset = AV_Lrs2_pickleDataset(data_test_list,re_fs,orig_fs)
-    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,batch_size=batch_size, shuffle=False,num_workers=8)
-    
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,batch_size=batch_size, collate_fn=lambda x:my_collate(x),shuffle=False,num_workers=8)
     model_test = UNet().to(device)
     model_test.load_state_dict(torch.load(test_model,map_location=device))
     model_test.eval()
     
     
     with torch.no_grad():
-        for i, (batch_data) in enumerate(test_loader):
+        for i, (data_name,data_wav_len,re_fs,data_wav,input_wav_real,input_wav_imag) in enumerate(test_loader):
             print(i)
-            
-            audio_real = batch_data["audio_data_Real"][0].to(device)
-            audio_imagine = batch_data["audio_data_Imagine"][0].to(device)
+            audio_real = input_wav_real.to(device)
+            audio_imagine = input_wav_imag.to(device)
             audio_maxlen = int(audio_real.shape[-1]*256*fs-1)
             
             enhance_r, enhance_i = model_test(audio_real,audio_imagine)
@@ -102,21 +102,18 @@ if __name__ == '__main__':
             enhance_i = enhance_i.unsqueeze(3)
             enhance_spec = torch.cat((enhance_r,enhance_i),3)
             audio_me_pe = complex_demand_audio(enhance_spec,window,audio_maxlen,re_fs)
-            
-            data_wav_len = batch_data["data_wav_len"][0]
-            
-            input_audio = batch_data["audio_wav"][0]
-            input_audio = tensor2audio(input_audio,window,length=data_wav_len)
-
-            
+            print(audio_me_pe)
+       
             audiosave_path = test_data_output_path
             if not os.path.exists(audiosave_path):
                 os.makedirs(audiosave_path)
-             
-            data_name = batch_data["data_name"][0]
-            re_sr = batch_data["sr"][0]
-            torchaudio.save(audiosave_path+"/"+data_name+".wav", src=torch.from_numpy(audio_me_pe[:data_wav_len]).unsqueeze(0), sample_rate=int(16000*re_sr))
+            
+            data_name = data_name[0]
+            re_sr = re_fs
+            audio_me_pe=audio_me_pe.to('cpu')
 
+            #torchaudio.save(audiosave_path+"/"+data_name+".wav", src=torch.from_numpy(audio_me_pe[:data_wav_len]).unsqueeze(0), sample_rate=int(16000*re_sr))
+            torchaudio.save(audiosave_path+"/"+data_name+".wav", src=audio_me_pe[:int(data_wav_len)], sample_rate=int(16000*re_sr))
 
 
 
